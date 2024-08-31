3252d886157f5e32bc90431d6e583e709d5dcc24
Encore <encore.shao@gmail.com>
2024-08-31 09:46:24 +0800

    feat: add quote-master screenshots and update page content

--- js/quote-master.js
+++ js/quote-master.js
@@ -35,7 +35,7 @@ $(document).on("click", 'a[href^="#"]', function(event) {
 
 // When the user scrolls down 20px from the top of the document, show the scroll up button
 window.onscroll = function() {
-  scrollFunction();
+  // scrollFunction();
 };
 
 function scrollFunction() {
